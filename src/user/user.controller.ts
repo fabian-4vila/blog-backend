952801a7106506de07db49aadf4c37a91e9d7538
Fabian <fabianavb22@gmail.com>
2025-01-18 15:25:20 -0500

    esqueleto-modelos

---
+++ src/user/user.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+
+class UserController { 
+    constructor () {
+
+    }
+    /**
+     * getAllUsers
+     */
+    public getAllUsers= async(_req:Request, res:Response) => {
+        res.status(200).json({
+            ok:true,
+            message:`lista de usuarios`
+        });
+    };
+
+    /**
+     * getUserById
+     * */
+    public getUserById = async (req:Request, res:Response) => {
+        const {id:userId} = req.params;
+        console.log(`userId: ${userId}`);
+        res.status(200).json({
+            ok:true,
+            message:`Detalles de usuario `
+        });
+    };
+    
+    /**
+     * createUser
+     * */
+    public createUser= async (req:Request,res:Response) =>  {
+        console.log("Body de la req",req.body);
+            
+            res.status(200).json({
+                ok:true,
+                message:`Usuario creado `
+            });
+    }
+
+    /**
+     * updateUserById
+     */
+    public updateUserById = async (_req:Request ,res:Response) => {
+        
+    }
+};
+
+export default UserController
