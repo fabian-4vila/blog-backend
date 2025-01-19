f5e8d2d5d88b0aa04b586576d4d813d0855600a3
Fabian <fabianavb22@gmail.com>
2025-01-19 12:59:06 -0500

    service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
+import UserService from "./user.service";
+import { logger } from "../utils/logger";
 
 class UserController { 
+    private readonly userService: UserService = new UserService
     constructor () {
 
     }
@@ -8,8 +11,11 @@ class UserController {
      * getAllUsers
      */
     public getAllUsers= async(_req:Request, res:Response) => {
-        res.status(200).json({
+        logger.info(`${UserController.name}-getAllUser`);
+        const usersResp =await this.userService.getAllUser();
+        res.json({
             ok:true,
+            users:usersResp,
             message:`lista de usuarios`
         });
     };
@@ -19,9 +25,11 @@ class UserController {
      * */
     public getUserById = async (req:Request, res:Response) => {
         const {id:userId} = req.params;
-        console.log(`userId: ${userId}`);
+        logger.info(`${UserController.name}-getUserById`);
+        const user =  await this.userService.getUserById(userId)
         res.status(200).json({
             ok:true,
+            user,
             message:`Detalles de usuario `
         });
     };
@@ -29,10 +37,12 @@ class UserController {
     /**
      * createUser
      * */
-    public createUser= async (req:Request,res:Response) =>  {
-        console.log("Body de la req",req.body);
+    public createUser= async (_req:Request,res:Response) =>  {
+        logger.info(`${UserController.name}-getUserById`);
+        const user = await this.userService.createUser();
             res.status(200).json({
                 ok:true,
+                user,
                 message:`Usuario creado `
             });
     }
