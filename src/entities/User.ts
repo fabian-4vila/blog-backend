9fa135ecd443fd045fc9ffb7b6f2c7547de8a1fb
Fabian <fabianavb22@gmail.com>
2025-01-25 15:23:56 -0500

    cambio de las tablas1

---
+++ src/entities/User.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Post } from "./Posteo";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "varchar", length: 100 })
+  name!: string; // Nombre del usuario
+
+  @Column({ type: "varchar", length: 150, unique: true })
+  email!: string; // Correo electrónico único
+
+  @Column({ type: "varchar", length: 255 })
+  password!: string; // Contraseña del usuario
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  registeredAt!: Date; // Fecha de registro del usuario
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+  updatedAt!: Date; // Fecha de última actualización
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts!: Post[]; // Lista de posteos creados por el usuario
+
+  @OneToMany(() => Comment, (comment) => comment.user)
+  comments!: Comment[]; // Lista de comentarios hechos por el usuario
+}
