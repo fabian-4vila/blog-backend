ce6826a863a4be3b563a29054277c177ae5f033f
Fabian <fabianavb22@gmail.com>
2025-01-25 22:51:57 -0500

    cambio-de-las-tablas-solucionado

---
+++ src/entities/CommentRating.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Unique, Check, PrimaryGeneratedColumn, ManyToOne, Column } from 'typeorm';
+import { User } from './User.entity';
+import { Comment } from './Comment.entity';
+
+@Entity('comment_ratings')
+@Unique(['user', 'comment'])
+@Check('stars >= 1 AND stars <= 5')
+export class CommentRating {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => Comment, (comment) => comment.ratings, { nullable: false, onDelete: 'CASCADE' })
+  comment!: Comment; // Comentario calificado
+
+  @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
+  user!: User; // Usuario que realizó la calificación
+
+  @Column({ type: 'int' })
+  stars!: number; // Número de estrellas (1-5)
+
+  @Column({ type: 'boolean' })
+  likeDislike!: boolean; // Indica si es un "me gusta" o "no me gusta"
+}
---
+++ src/entities/PostRating.entity.ts
@@ -0,0 +1,23 @@
+import { Check, Column, Entity, ManyToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { Post } from './Post.entity';
+import { User } from './User.entity';
+
+@Entity('post_ratings')
+@Unique(['user', 'post'])
+@Check('stars >= 1 AND stars <= 5')
+export class PostRating {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => Post, (post) => post.ratings, { nullable: false, onDelete: 'CASCADE' })
+  post!: Post; // Posteo calificado
+
+  @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
+  user!: User; // Usuario que realizó la calificación
+
+  @Column({ type: 'int' })
+  stars!: number; // Número de estrellas (1-5)
+
+  @Column({ type: 'boolean' })
+  likeDislike!: boolean; // Indica si es un "me gusta" o "no me gusta"
+}
