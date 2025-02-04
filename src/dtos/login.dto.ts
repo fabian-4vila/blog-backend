add7ba038ca801fb3f484341265ecee2d75fba32
Fabian <fabianavb22@gmail.com>
2025-02-03 23:58:12 -0500

    errores-resueltos-authRoutes

--- src/dtos/login.dto.ts
+++ src/dtos/login.dto.ts
@@ -3,6 +3,6 @@ import { IsEmail, MinLength } from 'class-validator';
 export class LoginDto {
   @IsEmail({}, { message: 'The email is not valid' })
   email!: string;
-  @MinLength(6, { message: 'Password must be at least 6 characters' })
+  @MinLength(8, { message: 'Password must be at least 8 characters' })
   password!: string;
 }
