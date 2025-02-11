37dc02835f143d8320efba7ef58632239c494943
Fabian <fabianavb22@gmail.com>
2025-02-11 16:21:39 -0500

    autenticacion-y-permisos

--- src/entities/User.entity.ts
+++ src/entities/User.entity.ts
@@ -7,7 +7,7 @@ import { hashPassword } from '../utils/hash';
 
 @Entity({ name: 'user' })
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid') // Usar UUID en lugar de un número autoincremental
   id!: string;
 
   @Column({ type: 'varchar', length: 100 })
@@ -16,7 +16,7 @@ export class User {
   @Column({ type: 'varchar', length: 150, unique: true })
   email!: string;
 
-  @Exclude()
+  @Exclude() // Excluye la contraseña al serializar la entidad
   @Column({ type: 'varchar', length: 255 })
   password!: string;
 
@@ -25,18 +25,21 @@ export class User {
     this.password = await hashPassword(this.password);
   }
 
-  @Column({ type: 'enum', enum: RoleType, default: RoleType.GENERAL })
+  @Column({ type: 'enum', enum: RoleType, default: RoleType.SUBSCRIBED })
   role!: RoleType;
 
+  @Column({ type: 'jsonb', default: () => "'[]'::jsonb" })
+  permissions!: string[]; // Lista de permisos específicos del usuario
+
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  registeredAt!: Date; // Fecha de registro
+  registeredAt!: Date;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
-  updatedAt!: Date; // Fecha de última actualización
+  updatedAt!: Date;
 
   @OneToMany(() => Post, (post) => post.user)
-  post!: Post[]; // Lista de posteos creados
+  post!: Post[];
 
   @OneToMany(() => Comment, (comment) => comment.user)
-  comments!: Comment[]; // Lista de comentarios hechos
+  comments!: Comment[];
 }
